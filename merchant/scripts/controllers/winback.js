7b243f98210f68904888546398045bb27137722f
Jayram <jayram.chandan@gmail.com>
2014-10-16 13:38:04 +0530

    The winback ui

--- merchant/scripts/controllers/winback.js
+++ merchant/scripts/controllers/winback.js
@@ -14,6 +14,17 @@ twystApp.factory('winbackService', function ($http, $q) {
         return defer.promise;
     }
 
+    winSvc.update = function(winback) {
+
+        var defer = $q.defer();
+        $http.put(
+            '/api/v3/winback', winback
+        ).success(function (data) {
+            defer.resolve(data);
+        });
+        return defer.promise;
+    }
+
     winSvc.read = function(winback) {
 
         var defer = $q.defer();
@@ -28,8 +39,8 @@ twystApp.factory('winbackService', function ($http, $q) {
     winSvc.readOne = function(id) {
 
         var defer = $q.defer();
-        $http.post(
-            '/api/v3/winback/'+ winback
+        $http.get(
+            '/api/v3/winback/'+ id
         ).success(function (data) {
             defer.resolve(data);
         });
@@ -38,7 +49,7 @@ twystApp.factory('winbackService', function ($http, $q) {
 
     return winSvc;
 }).
-controller('WinbackCtrl', function ($scope, winbackService) {
+controller('WinbackCtrl', function ($scope, $location, $routeParams, winbackService) {
     $scope.winback = {};
     $scope.winback.offers = [{
         basics: {
@@ -54,10 +65,11 @@ controller('WinbackCtrl', function ($scope, winbackService) {
         {"text": "Flat off", value:"flat"},
         {"text": "Free ", value:"free"},
         {"text": "Buy one get one ", value:"buy_one_get_one"},
-        {"text": "Happy hours", value:"happy"},
+        {"text": "Happy hours", value:"happyhours"},
         {"text": "Reduced price ", value:"reduced"},
         {"text": "Custom ", value:"custom"}
     ];
+    $scope.preserve_reward = null;
     $scope.selected_reward = $scope.reward_check[0];
 
 
@@ -113,24 +125,73 @@ controller('WinbackCtrl', function ($scope, winbackService) {
         }
     };
 
+    $scope.correctReward = function (param) {
+        $scope.selected_reward = param;
+        if(!$scope.winback.offers[0]._id) {
+            $scope.offer.reward = {};
+        } else {
+            if($scope.winback.offers[0].reward) {
+                var key = Object.keys($scope.preserve_reward)[0];
+                if(param.value === key) {
+                    $scope.winback.offers[0].reward = $scope.preserve_reward;
+                }
+                else {
+                    $scope.winback.offers[0].reward = {};
+                }
+            } else {
+                $scope.winback.offers[0].reward = {};
+            }
+        }
+    }
+
     $scope.create = function () {
         $scope.winback.offers = getOffer();
         winbackService.create($scope.winback).then(function(data) {
-            console.log(data);
+            if(data.status = "success"){
+                $location.path("/winback");
+            }
+        })   
+    };
+
+    $scope.update = function () {
+        $scope.winback.offers = getOffer();
+        winbackService.update($scope.winback).then(function(data) {
+            if(data.status = "success"){
+                $location.path("/winback");
+            }
         })   
     };
 
     $scope.read = function () {
-        winbackService.read($scope.winback).then(function(data) {
-            console.log(data);
-        })   
+        winbackService.read().then(function(data) {
+            $scope.winbacks = data.info;
+        });
     };
 
-    $scope.readOne = function (id) {
+    $scope.readOne = function () {
+        var id = $routeParams.winback_id;
         winbackService.readOne(id).then(function(data) {
-            console.log(data);
-        })   
+            $scope.winback = data.info;
+            setOfferAttr($scope.winback.offers);
+        });  
     };
+
+    function setOfferAttr (offers) {
+        if(offers && offers.length > 0) {
+            var o = offers[0];
+            if(!o.reward) {
+                $scope.selected_reward = $scope.reward_check[0];
+            }
+            else {
+                var l = Object.keys(o.reward)[0];
+                var ll = _.find($scope.reward_check, function (reward){return reward.value === l});
+                $scope.selected_reward = ll;
+                $scope.preserve_reward = o.reward;
+            }
+            $scope.rewardCheckedTime = o.reward_applicability.time_of_day;
+            $scope.rewardCheckedDays = o.reward_applicability.day_of_week;
+        }
+    }
 
     function getOffer() {
         var offers = [],
@@ -140,6 +201,9 @@ controller('WinbackCtrl', function ($scope, winbackService) {
         o.reward_applicability = {};
         o.reward_applicability.time_of_day = $scope.rewardCheckedTime;
         o.reward_applicability.day_of_week = $scope.rewardCheckedDays;
+        if($scope.preserve_reward && !o.reward) {
+            o.reward = $scope.preserve_reward;
+        }
         offers.push(o);
         return offers;
     }
