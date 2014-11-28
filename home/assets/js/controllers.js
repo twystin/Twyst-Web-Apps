ba29b10b17a7d964e95171f6cb5d66ae70c53342
Jayram <jayram.chandan@gmail.com>
2014-11-28 16:49:02 +0530

    Download thing on footer

--- home/assets/js/controllers.js
+++ home/assets/js/controllers.js
@@ -49,6 +49,25 @@ angular.module('twystApp', ['ngAnimate'])
             $scope.results = data.info;
         })
     }
+
+    $scope.getMobileOperatingSystem = function () {
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+    if( userAgent.match( /iPad/i ) || userAgent.match( /iPhone/i ) || userAgent.match( /iPod/i ) )
+        {
+          return 'iOS';
+
+        }
+        else if( userAgent.match( /Android/i ) )
+        {
+
+          return 'Android';
+        }
+        else
+        {
+          return 'unknown';
+        }
+    }
 })
 .controller('ContactCtrl', function($scope, $http) {
     $scope.user = {};
