4addc4ada2d7fb1eadbb90f81ca12319004f164f
Jayram <jayram.chandan@gmail.com>
2014-11-28 18:45:18 +0530

    Discover page

--- discover/assets/js/discoverApp.js
+++ discover/assets/js/discoverApp.js
@@ -66,4 +66,21 @@ angular.module('discoverApp', [])
             $scope.total = data.info.total;
         })
     }
+
+    $scope.getMobileOperatingSystem = function () {
+        var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+        if( userAgent.match( /iPad/i ) || userAgent.match( /iPhone/i ) || userAgent.match( /iPod/i ) )
+        {
+          return 'iOS';
+
+        }
+        else if( userAgent.match( /Android/i ) )
+        {
+          return 'Android';
+        }
+        else
+        {
+          return 'unknown';
+        }
+    }
 });
